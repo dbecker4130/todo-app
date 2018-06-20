4e152bfe6667936dcf0cfe0efe8eb57bad194b2c
Daniel Becker <dbecker4130@gmail.com>
2018-06-20 14:08:51 -0700

    basic input form complete, material list to be implemented

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import PropTypes from 'prop-types';
 import autoBind from 'react-autobind';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+// import InsetList from './MaterialList';
 import './Reset.scss';
 import './App.scss';
+// import { Checkbox, List, ListItem, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
+
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+});
 
 class App extends Component {
     constructor() {
@@ -12,41 +23,44 @@ class App extends Component {
         autoBind(this);
         this.state = {  
             items: [],
-            text: ''
+            text: '',
+            checked: []
         };
     }
-
-    render() {
-        const { classes } = this.props;
-        return(
-            <div className="app">
-                <h1>SHIT LIST</h1>
+            
+        render() {
+            const { classes } = this.props;
+            return(
+                <div className="App">
                 <form onSubmit={ this.handleSubmit }>
+                    <h1>SHIT LIST</h1>
                     <input
                         id="new-text"
                         placeholder="what is it?"
                         onChange={ this.handleChange }
                         value={ this.state.text }
-                    />
+                        />
                     <Button
                         type="submit"
                         variant="outlined" 
                         className={classes.button}
-                    >
+                        >
                         Add #{ this.state.items.length + 1 }
                     </Button>
                 </form>
-                <List items={ this.state.items } />
+                <ListItems items={ this.state.items } />
+                {/* <InsetList classesList={ classesList } /> */}
+                
             </div>
         )
     }
-
+    
     handleChange(e) {
         this.setState({
             text: e.target.value
         });
     }
-
+    
     handleSubmit(e) {
         e.preventDefault();
         if (!this.state.text.length) {
@@ -67,7 +81,7 @@ App.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-class List extends React.Component {
+class ListItems extends React.Component {
     render() {
         return (
             <ul>
@@ -79,4 +93,4 @@ class List extends React.Component {
     }
 }
 
-export default withStyles()(App);
+export default withStyles(styles)(App);
