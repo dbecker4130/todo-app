412530e54a3e272cf2eee0dcc0c3bed2b1a65fc0
Daniel Becker <dbecker4130@gmail.com>
2018-06-22 12:20:40 -0700

    connected to mongo with env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const port = 8000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+const port = process.env.PORT;
+
 const app = express();
+
+// mongoose.connect(process.env.MONGODB_LOCAL);
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log(`app listening on port: ${port}`);
